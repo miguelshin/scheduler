18190daa8b7fc3fb4d94295cf689fbc77638c3ac
mortiz <miguelaob@gmail.com>
2017-10-22 14:07:10 +0200

    Firebase code to the service

--- public/js/loginScheduler.component.js
+++ public/js/loginScheduler.component.js
@@ -5,34 +5,18 @@ loginSchedulerApp.component('loginScheduler', {
 
 loginSchedulerApp.controller('LoginSchedulerCtrl', LoginSchedulerCtrl);
 
-function LoginSchedulerCtrl($scope) {
+function LoginSchedulerCtrl($scope, schedulerService) {
 	var self = this;
-	self.loginValidation = loginValidation;
-	self.logout = logout;
-	self.initializeFirebase = initializeFirebase;
 	self.$onInit = init;
+	self.schedulerService = schedulerService;	
 
 	function init() {
-		initializeFirebase();
-		//alert("XDDD");
-	}
-
-	function initializeFirebase() {
-		// Initialize Firebase
-		var config = {
-			apiKey: "AIzaSyDcOyjL7sL3ESHlIWmrTLkLl0cbSYTgBqk",
-			authDomain: "scheduler-ff919.firebaseapp.com",
-			databaseURL: "https://scheduler-ff919.firebaseio.com",
-			projectId: "scheduler-ff919",
-			storageBucket: "scheduler-ff919.appspot.com",
-			messagingSenderId: "241428894999"
-		};
-		if (firebase !== undefined) {
-			firebase.initializeApp(config);
-			loginValidation();
-		}
+		debugger;
+		firebase = schedulerService.initializeFirebase();
+		debugger;
 		firebase.auth().onAuthStateChanged(function(user) {
 			$scope.$apply(function() {
+				debugger;
 				if (user) {
 					self.validatedEmail = user.email;
 				} else {
@@ -40,27 +24,7 @@ function LoginSchedulerCtrl($scope) {
 				}
 			});
 		});
-	}
 
-	function loginValidation() {
-		// TODO: ponerlo en un servicio!!! ->
-		//firebase.auth().signInWithEmailAndPassword(this.email, this.password);
-		firebase.auth().signInWithEmailAndPassword("miguelaob@gmail.com", "123456");
 	}
-	function logout() {
-		firebase.auth().signOut().then(function() {
-			self.validatedEmail = null;
-		// Sign-out successful.
-		}, function(error) {
-		// An error happened.
-		});
-	}
-
-	/*firebase.auth().createUserWithEmailAndPassword("benitomillan@gmail.com", "123456").catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// ...
-	});*/
 
 }
--- public/js/scheduler.services.angular.js
+++ public/js/scheduler.services.angular.js
@@ -1,5 +1,5 @@
 // Webservice remote calling
-schedulerApp.service('schedulerService', function ($http) {
+schedulerApp.service('schedulerService', function ($http, $q) {
 
   this.year = function (callback, errorHandler, year) {
   	$http.get('/year/' + year).then(callback, errorHandler);
@@ -8,5 +8,48 @@ schedulerApp.service('schedulerService', function ($http) {
   this.day = function(year, month, day) {
  	$http.get("/day/" + year + "/" + month + "/" + day).then(callback, errorHandler);
   };
+
+  this.validatedEmail = null;
+  
+	this.initializeFirebase = function() {
+    debugger;
+		// Initialize Firebase
+		var config = {
+			apiKey: "AIzaSyDcOyjL7sL3ESHlIWmrTLkLl0cbSYTgBqk",
+			authDomain: "scheduler-ff919.firebaseapp.com",
+			databaseURL: "https://scheduler-ff919.firebaseio.com",
+			projectId: "scheduler-ff919",
+			storageBucket: "scheduler-ff919.appspot.com",
+			messagingSenderId: "241428894999"
+		};
+		if (firebase !== undefined) {
+			firebase.initializeApp(config);
+			this.loginValidation();
+		}
+		return firebase;
+	}
+
+	this.loginValidation = function() {
+		debugger;
+		// TODO: ponerlo en un servicio!!! ->
+		//firebase.auth().signInWithEmailAndPassword(this.email, this.password);
+		firebase.auth().signInWithEmailAndPassword("miguelaob@gmail.com", "123456");
+  };
+  
+	this.logout = function() {
+		firebase.auth().signOut().then(function() {
+			self.validatedEmail = null;
+		// Sign-out successful.
+		}, function(error) {
+		// An error happened.
+		});
+	};
+
+	/*firebase.auth().createUserWithEmailAndPassword("benitomillan@gmail.com", "123456").catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// ...
+	});*/
 
 });
